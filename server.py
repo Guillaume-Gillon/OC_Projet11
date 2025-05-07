6dfa3fbe98a3536d825e815c58da38b4568b3b27
Guillaume <guillaume.gillon@protonmail.com>
2025-05-07 20:03:13 +0200

    WIP

--- server.py
+++ server.py
@@ -56,9 +56,16 @@ def purchasePlaces():
     ]
     club = [c for c in clubs if c["name"] == request.form["club"]][0]
     placesRequired = int(request.form["places"])
-    competition["numberOfPlaces"] = int(competition["numberOfPlaces"]) - placesRequired
-    flash("Great-booking complete!")
-    return render_template("welcome.html", club=club, competitions=competitions)
+    if placesRequired > int(club["points"]):
+        flash("Not enough points to complete the booking.")
+        return render_template("welcome.html", club=club, competitions=competitions)
+    else:
+        competition["numberOfPlaces"] = (
+            int(competition["numberOfPlaces"]) - placesRequired
+        )
+        club["points"] = int(club["points"]) - placesRequired
+        flash("Great-booking complete!")
+        return render_template("welcome.html", club=club, competitions=competitions)
 
 
 # TODO: Add route for points display
